85d75e563ebca50566917ec7dcfef2eeae56165a
Patricio Bruna <pbruna@gmail.com>
2011-12-19 11:05:25 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+POSAdmin::Application.config.secret_token = '84f1d997711c4e12c1a73a7ddec53dc3b85bc02be7b400b1f9672d9c881350f9f3cb8feb8fb6c4bdfa98c585fb06ae5fff84a3d40b0adab4a62449f6d640848f'
